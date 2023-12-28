00138e0bbf497d655fab602d88f2813b99461c5b
Ivan Kovalenko <dog161.2003@mail.ru>
2023-12-28 09:22:01 +0300

    test

--- labs/Lab_12_python_intro/solution/task1.py
+++ labs/Lab_12_python_intro/solution/task1.py
@@ -1,15 +1,12 @@
 def count_routes(N, M):
-    mas = [[0] * (M+1) for _ in range(N+1)]
-    mas[1][1] = 1
-
+    arr = [[0] * (M+1) for _ in range(N+1)]
+    arr[1][1] = 1
     for i in range(2, N + 1):
         for j in range(2, M + 1):
-            mas[i][j] = mas[i - 1][j - 2] + mas[i - 2][j - 1]
-
-    return mas[N][M]
-
+            arr[i][j] = arr[i - 1][j - 2] + arr[i - 2][j - 1]
+    return arr[N][M]
 if __name__ == '__main__':
-    N = int(input("Введите количество строк N : "))
-    M = int(input("Введите количество столбцов M : "))
+    N = int(input("Enter number of lines N : "))
+    M = int(input("Enter number of columns M : "))
     result = count_routes(N, M)
-    print("Количество различных маршрутов: ", result)
+    print("Number of different routes: ", result)
